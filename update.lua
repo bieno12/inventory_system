842240a7a5b028dc0a40d5c2f7eaeea5442a30a1
bieno12 <zeyadshahin2015@gmail.com>
2023-06-27 22:51:30 +0300

    updated update

--- update.lua
+++ update.lua
@@ -4,6 +4,7 @@ local DIR_PREFIX = '/inventory_system/'
 local function entry(name)
     return function()
         response = http.get(URL_PREFIX .. name, nil, true)
+        print(("%s : %d"):format(name, response.getResponseCode()))
         local file = fs.open(DIR_PREFIX .. name, 'wb')
         file.write(response.readAll())
         file.close()
